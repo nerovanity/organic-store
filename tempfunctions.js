ea91f2426ddcac9040234b0cce16584d667c211e
nerovanity <watashiwaningida@gmail.com>
2023-05-10 15:55:24 +0100

    .

--- tempfunctions.js
+++ tempfunctions.js
@@ -1,4 +1,32 @@
+
+
 function vegetables(){
-    const not_vegetables = document.getElementsByid('not_vegetables');
-    not_vegetables.classList.toggle('hide_it');
-  }
+    const not_vegetables = document.querySelectorAll('.not-vegetables');
+    not_vegetables.forEach(function(element) {
+        element.classList.toggle("hide_it");
+    });
+}
+function fruits(){
+    const not_fruit = document.querySelectorAll('.not-fruit');
+    not_fruit.forEach(function(element) {
+        element.classList.toggle("hide_it");
+    });
+}
+function other(){
+    const not_other = document.querySelectorAll('.not-other');
+    not_other.forEach(function(element) {
+        element.classList.toggle("hide_it");
+    });
+}
+function beauty(){
+    const not_beauty = document.querySelectorAll('.not-beauty');
+    not_beauty.forEach(function(element) {
+        element.classList.toggle("hide_it");
+    });
+}
+function allpro(){
+    const allpro=document.querySelectorAll('.productone, .producttwo, .productthree, .productfour, .productfive, .productsix, .productseven, .producteight');
+    allpro.forEach(function(element) {
+        element.classList.remove("hide_it");
+    });
+}
